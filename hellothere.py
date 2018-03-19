dface2f52f388ddbac87859d4676266585a4fabd
Darrel Adams <darrel.adams8@gmail.com>
2018-03-18 22:24:35 -0700

    Add files via upload

---
+++ hellothere.py
@@ -0,0 +1,14 @@
+def hello():
+    print('Howdy!')
+    print('Howdy!!')
+    print('Hello there.')
+
+hello()
+hello()
+hello()
+
+def hi(name):
+    print('Hello ' + name)
+
+hi('Darrel')
+hi('The Adams family')
